444c46fe87dda3b873f2808c2ac325495086adf8
Endsmasher <endsmeshaa@gmail.com>
2021-12-06 20:16:15 +0100

    bugfix and tested 1.18

--- src/main/java/de/endsmasher/pricedteleport/listener/TeleportWarpListener.java
+++ src/main/java/de/endsmasher/pricedteleport/listener/TeleportWarpListener.java
@@ -5,6 +5,10 @@ import de.endsmasher.pricedteleport.event.TeleportWarpEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TeleportWarpListener implements Listener {
 
@@ -20,15 +24,9 @@ public class TeleportWarpListener implements Listener {
 
         if(!player.hasPermission("ptp.bypass")) {
             if (!navigator.getItemStacks().isEmpty()) {
-                for (ItemStack itemStack : navigator.getItemStacks()) {
-
-                    if (!player.getInventory().contains(itemStack)) {
-                        player.sendMessage(PricedTeleport.PREFIX + "You don't meet the requirements");
-                        break;
-                    }
-                }
-                for (ItemStack itemStack : navigator.getItemStacks()) {
-                    player.getInventory().remove(itemStack);
+                if (!checkAndRemoveItems(player.getInventory(), navigator.getItemStacks())) {
+                    player.sendMessage(PricedTeleport.PREFIX + "You don't meet the requirements");
+                    return;
                 }
             }
         }
@@ -36,4 +34,57 @@ public class TeleportWarpListener implements Listener {
         player.teleport(location);
         player.sendMessage(PricedTeleport.PREFIX + "You have been teleported to " + navigator.getName());
     }
+
+    private boolean checkAndRemoveItems(PlayerInventory playerInventory, List<ItemStack> requiredItems) {
+        var i = -1;
+        var items = requiredItems.stream().collect(Collectors.toMap(itemStack -> itemStack, ItemStack::getAmount, Integer::sum, HashMap::new));
+        var slotsToChange = new HashMap<Integer, Integer>();
+
+        for (ItemStack itemStack : playerInventory.getContents()) {
+            i++;
+            if (itemStack == null) continue;
+            for (var requiredItem : new ArrayList<>(items.entrySet())) {
+                if (!itemsMatch(itemStack, requiredItem.getKey())) continue;
+                var missing = requiredItem.getValue();
+                missing -= slotsToChange.getOrDefault(i, itemStack.getAmount());
+                if (missing < 0) {
+                    items.remove(requiredItem.getKey());
+                    slotsToChange.put(i, Math.abs(missing));
+                    continue;
+                }
+                items.put(requiredItem.getKey(), missing);
+                if (missing == 0) {
+                    items.remove(requiredItem.getKey());
+                }
+                slotsToChange.put(i, 0);
+                break;
+            }
+        }
+        if (items.size() > 0) {
+            return false;
+        }
+        slotsToChange.forEach((slot, amount) -> {
+            if (amount <= 0) {
+                playerInventory.setItem(slot, null);
+                return;
+            }
+            Objects.requireNonNull(playerInventory.getItem(slot)).setAmount(amount);
+        });
+        return true;
+    }
+
+    private boolean itemsMatch(ItemStack itemStack, ItemStack requiredItem) {
+        if (itemStack.getType() != requiredItem.getType()) return  false;
+        if (!Objects.equals(extractName(itemStack), extractName(requiredItem))) return false;
+        // todo meta stuff
+        return true;
+    }
+
+    private String extractName(ItemStack itemStack) {
+        var meta = itemStack.getItemMeta();
+        if (meta == null) return null;
+        if (!meta.hasDisplayName()) return null;
+        return meta.getDisplayName();
+    }
+
 }
